bb5016a3ab68b12316396cf5dfcfa6bef75d52ad
maurits444 <144318508+maurits444@users.noreply.github.com>
2024-06-26 16:41:22 +0200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,61 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "maincharachter":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . 1 1 1 . . . . . 
+. . . . . . . 1 f f f f . . . . 
+. . . . . . 1 f f f f f f f . . 
+. . . . . 1 f f f f f f f f f . 
+. . . . 1 f f f f 1 f f f f f f 
+. . . 1 f f f 1 f f 1 f f f f f 
+. . 1 f f 1 f f 1 f f 1 f f f f 
+f 1 f f f f 1 f f 1 f f 1 f f f 
+f f f f f f f f f f f f f f f f 
+e e e e e e e e e e e e e e e e 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . e f f . . . . . . . . 
+. . . . . e f 1 . . . . . . . . 
+. . . . . e f f 1 . . . . . . . 
+. . . . . e f f f 1 . . . . . . 
+. . . . . e f f f f 1 . . . . . 
+. . . . . e f f 1 f f 1 . . . . 
+. . . . . e f 1 f f f f 1 . . . 
+. . . . . e f f f 1 f f f 1 . . 
+. . . . . e f f 1 f f f f f 1 . 
+. . . . . e f 1 f f 1 f f f 1 . 
+. . . . . e f f f 1 f f f f 1 . 
+. . . . . e f f 1 f f f f f . . 
+. . . . . e f 1 f f f f f . . . 
+. . . . . e f f f f f f f . . . 
+. . . . . e f f f f f f . . . . 
+. . . . . e f f f f f . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,27 @@
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+animation.runImageAnimation(
+mySprite,
+assets.animation`maincharachter`,
+500,
+true
+)
+game.onUpdate(function () {
+    controller.moveSprite(mySprite, 200, 200)
+})
